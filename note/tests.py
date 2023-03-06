c259ae4783e3f765776ba52883ebae67b1979604
JosueM <paredesjosue77@gmail.com>
2023-03-05 20:52:45 -0600

    Agregando test

--- note/tests.py
+++ note/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Course
 
-# Create your tests here.
+
+class CourseTest(TestCase):
+    def setUp(self):
+        Course.objects.create(title='Curso Django', technology='Python')
+
+    def test_course_find_Django(self):
+        course1 = Course.objects.get(title='Curso Django')
+        self.assertEqual(course1.title, 'Curso Django')
